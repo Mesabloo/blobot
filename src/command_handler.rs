48e7a1f8965b5745c0fcb21c5e08e689e34d9c95
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-12-08 12:16:47 +0100

    :sparkles: Added beginning of command handling

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -1,5 +1,27 @@
+use serenity::prelude::{Context};
 use serenity::model::channel::Message;
+use log::{debug};
+use std::env;
 
-pub fn handle(_msg: Message) {
+pub fn handle(ctx: Context, msg: Message) {
+    debug!("Received message!");
+    debug!("> Is in guild channel?");
 
+    let channel = msg.channel_id.to_channel(&ctx).expect("channel not found");
+    if let Some(lock) = channel.guild() {
+        debug!("| Yes");
+    } else {
+        debug!("| No");
+        return;
+    }
+
+    debug!("> Is command?");
+    let mut content = msg.content;
+    let command_prefix = env::var("PREFIX").expect("command prefix");
+    if content.starts_with(&command_prefix) {
+        debug!("| Yes");
+    } else {
+        debug!("| No");
+        return;
+    }
 }
--- src/events/message_create.rs
+++ src/events/message_create.rs
@@ -4,10 +4,10 @@ use std::thread;
 
 use crate::command_handler;
 
-pub fn on_message(_ctx: Context, msg: Message) {
+pub fn on_message(ctx: Context, msg: Message) {
     if !msg.author.bot {
         thread::spawn(||
-            command_handler::handle(msg)
+            command_handler::handle(ctx, msg)
         );
     }
 }
